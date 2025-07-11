b419b84fee0974654825ab6636ff133f343b8de4
HeHHeyboi <thanathatwave@gmail.com>
2025-07-11 12:57:41 +0700

    Add NegativeTest

--- Lab3/RomanNumerals/src/test/java/RomanNum/NegativeTest.java
+++ Lab3/RomanNumerals/src/test/java/RomanNum/NegativeTest.java
@@ -4,9 +4,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class NegativeTest {
+	RomanNumerals rn = new RomanNumerals();
+
 	@Test
 	void invalid_digits() {
-
+		assertNull(rn.convertRomanNumToInt("J"));
+		// assertThrows(NullPointerException.class, () -> {
+		// rn.convertRomanNumToInt("J");
+		// });
 	}
 
+	@Test
+	void wrong_repeating_digits() {
+		assertNotEquals(10, rn.convertRomanNumToInt("VV"));
+		assertNotEquals(100, rn.convertRomanNumToInt("LL"));
+		assertNotEquals(1000, rn.convertRomanNumToInt("DD"));
+	}
+
+	@Test
+	void three_repeat_digits() {
+		assertNotEquals(4, rn.convertRomanNumToInt("IIII"));
+		assertNotEquals(40, rn.convertRomanNumToInt("XXXX"));
+		assertNotEquals(400, rn.convertRomanNumToInt("CCCC"));
+	}
 }
