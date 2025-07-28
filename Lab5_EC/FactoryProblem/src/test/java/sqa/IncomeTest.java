5a9e9d432a8992b91311254985fe0eae13309ee5
HeHHeyboi <thanathatwave@gmail.com>
2025-07-28 13:54:10 +0700

    Finish Lab5

---
+++ Lab5_EC/FactoryProblem/src/test/java/sqa/IncomeTest.java
@@ -0,0 +1,16 @@
+package sqa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class IncomeTest {
+	Income i = new Income();
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/IncomeTestData.csv", numLinesToSkip = 1)
+	void TestIncome(int num_impeller, int num_motor, int num_cover, int expectedIncome) {
+		assertEquals(expectedIncome, i.calculateIncome(num_impeller, num_motor, num_cover));
+	}
+}
