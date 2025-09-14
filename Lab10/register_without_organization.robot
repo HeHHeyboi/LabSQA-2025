3554452aeae84b80ad7da4fd189b8be9d3e00e9d
HeHHeyboi <thanathatwave@gmail.com>
2025-09-14 14:47:36 +0700

    Create Server for test & 2 Test Scenario

---
+++ Lab10/register_without_organization.robot
@@ -0,0 +1,28 @@
+*** Settings ***
+Documentation  A test for use Robot
+Library  SeleniumLibrary
+
+*** Variables ***
+${CHROME_BROWSER_PATH}  ${CURDIR}${/}chrome-win64${/}chrome.exe
+${CHROME_DIRVER_PATH}  ${CURDIR}${/}chromedriver${/}chromedriver-win64${/}chromedriver.exe
+${URL}  http://localhost:8080/Lab10
+${BROWSER}  Chrome
+${DELAY}  3 seconds
+
+&{U1}  firstname=Somsri  lastname=sodsai  email=somsri@kkumail.com  phone=081-001-1234
+
+*** Test Cases ***
+Open Register web
+	Set Selenium Speed  ${DELAY}
+	Open Browser  ${URL}  ${BROWSER}
+
+Register without Organization
+	Set Selenium Speed  0s
+	FOR  ${key}  ${value}  IN  &{U1}
+		Input Text  ${key}  ${value}  true
+	END
+	Set Selenium Speed  1s
+	Submit Form  myform
+	${title} =  Get Title
+	Log To Console  ${title}
+	Title Should be  Success
---
+++ Lab10/web/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserData struct {
+	FirstName    string `form:"firstname"`
+	LastName     string `form:"lastname"`
+	Organization string `form:"organization"`
+	Email        string `form:"email"`
+	Phone        string `form:"phone"`
+}
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLFiles("./Registration.html", "./Success.html")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Server is on")
+	})
+	router.GET("/Lab10", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "Registration.html", nil)
+	})
+	router.POST("/Lab10", func(ctx *gin.Context) {
+		var u UserData
+		ctx.Bind(&u)
+		fmt.Printf("%v\n\n%+v\n\n%#v\n\n%T\n", u, u, u, u)
+		ctx.HTML(http.StatusOK, "Success.html", gin.H{})
+	})
+
+	router.Run("localhost:8080")
+}
